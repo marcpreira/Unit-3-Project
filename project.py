dd531940a3b6803f72ef16490f1ef0ff6aa05f28
marcpreira <marc.preira@daviestech.org>
2020-12-01 01:31:09 +0000

    Initial commit

---
+++ project.py
@@ -0,0 +1,17 @@
+name= input("Enter a name: ")
+place= input("Enter a place: " )
+adj1= input("Enter an adjective: ")
+adj2= input("Enter an adjective: ")
+food1= input("Enter a food: ")
+food2= input("Enter a food: ")
+adverb= input("Enter an adverb: ")
+verb= input("Enter a verb: ")
+noun= input("Enter a noun: ")
+print("Let's play Silly Sentences!\n ")
+
+print(name + " was planning a dream vacation to " + place + ".")
+print(name + " was especially looking forward to trying the local\ncuisine, including " + adj1  +  food1  + " and " + food2 + ".\n")
+
+print(name + " will have to practice the language " +  adverb  + " to \nmake it easier to " + verb + " with people.\n")
+
+print(name + " has a long list of sights to see, including the\n" + noun + " museum and the " + adj2 + " park.")
